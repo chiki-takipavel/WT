f3264e575fe38bbe225a5ecef56263809673e6fe
Pavel Miskevich <chiki-takipavel@mail.ru>
2020-03-26 22:41:15 +0300

    Добавлена лабораторная работа №2

---
+++ nav.php
@@ -0,0 +1,23 @@
+<?php
+$menu = array('index.php' => 'Главная', 'about.php' => 'О проекте');
+if (isset($_GET['url']))
+    $url = $_GET['url'];
+else
+    $url = '';
+?>
+
+<nav>
+    <ul class="nav-links">
+        <?php foreach ($menu as $key => $menuItem) : ?>
+            <li>
+                <?php
+                if ($key <> $url) { ?>
+                    <a href="<?= "/$key?url=$key" ?>"><?= $menuItem ?></a>
+                <?php } elseif ($key == $url) { ?>
+                    <a class="active" href="#"><?= $menuItem ?></a>
+                <?php }
+                ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</nav>
--- students/derivatives.php
+++ students/derivatives.php
@@ -1,31 +1,32 @@
 <!DOCTYPE html>
 <html lang="ru">
-    <head>
-        <meta charset='utf-8'>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Дифференциальное исчисление</title>
-        <link rel="stylesheet" type="text/css" href="../style.css">
-        <link rel="stylesheet" type="text/css" href="../style_math_data.css">
-        <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-        <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
-    </head>
 
-    <body>
-        <div class="wrapper">
-            <div class="content">
-                <header>
-                    <?php include('../nav.html') ?>
-                    <a class="button-signin" href="../signin.php"><button>Войти</button></a>    
-                </header>
+<head>
+    <meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Дифференциальное исчисление</title>
+    <link rel="stylesheet" type="text/css" href="../style.css">
+    <link rel="stylesheet" type="text/css" href="../style_math_data.css">
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+    <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+</head>
 
-                <div class="main">
-                    <div class="widget">
-                        <ul class="widget-list">
-                            <li><a href="/students/derivatives-1.php">Определение производной и дифференциала</a></li>
-                            <li><a href="#">Исследование функции с помощью производной</a></li>
-                            <li><a href="#">Некоторые приложения производной</a></li>
-                            <li><a href="#">Понятие функции нескольких переменных</a></li>
-                            <li><a href="#">Частные производные и дифференциалы</a></li>
-                            <li><a href="#">Экстремум функции нескольких переменных</a></li>
-                        </ul>
-                    </div>
+<body>
+    <div class="wrapper">
+        <div class="content">
+            <header>
+                <?php include('../nav.php') ?>
+                <a class="button-signin" href="../signin.php"><button>Войти</button></a>
+            </header>
+
+            <div class="main">
+                <div class="widget">
+                    <ul class="widget-list">
+                        <li><a href="/students/derivatives-1.php">Определение производной и дифференциала</a></li>
+                        <li><a href="#">Исследование функции с помощью производной</a></li>
+                        <li><a href="#">Некоторые приложения производной</a></li>
+                        <li><a href="#">Понятие функции нескольких переменных</a></li>
+                        <li><a href="#">Частные производные и дифференциалы</a></li>
+                        <li><a href="#">Экстремум функции нескольких переменных</a></li>
+                    </ul>
+                </div>
