16add5942bffaff4620be480f080b108bd45d18f
Pavel Miskevich <chiki-takipavel@mail.ru>
2020-04-03 19:59:18 +0300

    Мелкие исправления

--- index.php
+++ index.php
@@ -1,42 +1,44 @@
 <!DOCTYPE html>
 <html lang="ru">
-    <head>
-        <meta charset='utf-8'>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Математика</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
-    </head>
 
-    <body>
-        <div class="wrapper">
-            <div class="content">
-                <header>
-                    <?php include('nav.html') ?>
-                    <a class="button-signin" href="signin.php"><button>Войти</button></a>    
-                </header>
+<head>
+    <meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Математика</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 
-                <div class="home" >
-                    <h1 class="home-title">MATHEMATICS</h1>
-                </div>
+<body>
+    <div class="wrapper">
+        <div class="content">
+            <header>
+                <?php include('nav.php') ?>
+                <a class="button-signin" href="signin.php"><button>Войти</button></a>
+            </header>
 
-                <div class="choose-training">
-                    <a class="item-training" href="schoolchildren.php">
-                        <button>
-                            <img src="assets/img/For schoolchildren.svg" alt="Parabola">
-                            <h3>Для школьников</h3> 
-                        </button>
-                    </a>
-                    <a class="item-training" href="students.php">
-                        <button>
-                            <img src="assets/img/For students.svg" alt="Function">
-                            <h3>Для студентов</h3> 
-                        </button>
-                    </a>
-                </div>
+            <div class="home">
+                <h1 class="home-title">MATHEMATICS</h1>
             </div>
 
-            <?php include('footer.html') ?>
+            <div class="choose-training">
+                <a class="item-training" href="schoolchildren.php">
+                    <button>
+                        <img src="assets/img/For schoolchildren.svg" alt="Parabola">
+                        <h2>Для школьников</h2>
+                    </button>
+                </a>
+                <a class="item-training" href="students.php">
+                    <button>
+                        <img src="assets/img/For students.svg" alt="Function">
+                        <h2>Для студентов</h2>
+                    </button>
+                </a>
+            </div>
         </div>
 
-    </body>
+        <?php include('footer.html') ?>
+    </div>
+
+</body>
+
 </html>
--- schoolchildren.php
+++ schoolchildren.php
@@ -1,58 +1,60 @@
 <!DOCTYPE html>
 <html lang="ru">
-    <head>
-        <meta charset='utf-8'>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Для школьников</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
-    </head>
 
-    <body>
-        <div class="wrapper">
-            <div class="content">
-                <header>
-                    <?php include('nav.html') ?>
-                    <a class="button-signin" href="signin.php"><button>Войти</button></a>    
-                </header>
+<head>
+    <meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Для школьников</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 
-                <div class="training" >
-                    <h1 class="training-title">Для школьников</h1>
-                </div>
+<body>
+    <div class="wrapper">
+        <div class="content">
+            <header>
+                <?php include('nav.php') ?>
+                <a class="button-signin" href="signin.php"><button>Войти</button></a>
+            </header>
 
-                <div class="choose-topic">
-                    <a class="item-topic" href="#">
-                        <button>Числовые множества</button>
-                    </a>
-                    <a class="item-topic" href="#">
-                        <button>Функции</button>
-                    </a>
-                    <a class="item-topic" href="#">
-                        <button>Тождественные преобразования алгебраических выражений</button>
-                    </a>
-                    <a class="item-topic" href="#">
-                        <button>Тождественные преобразования тригонометрических выражений</button>
-                    </a>
-                    <a class="item-topic" href="#">
-                        <button>Уравнения</button>
-                    </a>
-                    <a class="item-topic" href="#">
-                        <button>Прогрессии</button>
-                    </a>
-                    <a class="item-topic" href="#">
-                        <button>Неравенства</button>
-                    </a>
-                    <a class="item-topic" href="#">
-                        <button>Планиметрия</button>
-                    </a>
-                    <a class="item-topic" href="#">
-                        <button>Стереометрия</button>
-                    </a>
-                </div>
-
+            <div class="training">
+                <h1 class="training-title">Для школьников</h1>
             </div>
-            
-            <?php include('footer.html') ?>
+
+            <div class="choose-topic">
+                <a class="item-topic" href="#">
+                    <button>Числовые множества</button>
+                </a>
+                <a class="item-topic" href="#">
+                    <button>Функции</button>
+                </a>
+                <a class="item-topic" href="#">
+                    <button>Тождественные преобразования алгебраических выражений</button>
+                </a>
+                <a class="item-topic" href="#">
+                    <button>Тождественные преобразования тригонометрических выражений</button>
+                </a>
+                <a class="item-topic" href="#">
+                    <button>Уравнения</button>
+                </a>
+                <a class="item-topic" href="#">
+                    <button>Прогрессии</button>
+                </a>
+                <a class="item-topic" href="#">
+                    <button>Неравенства</button>
+                </a>
+                <a class="item-topic" href="#">
+                    <button>Планиметрия</button>
+                </a>
+                <a class="item-topic" href="#">
+                    <button>Стереометрия</button>
+                </a>
+            </div>
+
         </div>
 
-    </body>
+        <?php include('footer.html') ?>
+    </div>
+
+</body>
+
 </html>
