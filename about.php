f9d8ff3f3c18cb864627cf4ac2fb04b1d8171c72
Pavel Miskevich <chiki-takipavel@mail.ru>
2020-04-03 20:10:30 +0300

    Добавлена страница "О проекте"

---
+++ about.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Математика</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="content">
+            <header>
+                <?php include('nav.php') ?>
+                <a class="button-signin" href="signin.php"><button>Войти</button></a>
+            </header>
+
+            <div class="about-wrapper">
+                <div class="about-content">
+                    <div class="photo-side">
+                        <img class="photo-developer" src="assets/img/Developer.jpg" alt="Developer"></img>
+                    </div>
+                    <div class="info-side">
+                        <p>Это просто лабораторная работа по веб-технологиям. Лучше введите названия городов через запятую.</p>
+                        <form method="POST">
+                            <input type="text" name="cities" placeholder="Введите города" required />
+                            <input type="submit" value="Отсортировать">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php include('footer.html') ?>
+    </div>
+</body>
+
+</html>
