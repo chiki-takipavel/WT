6724a6feedea9fa7e86678fd2a184d3ff341b1fb
Pavel Miskevich <chiki-takipavel@mail.ru>
2020-04-06 22:28:02 +0300

    Несколько исправлений

--- signin.php
+++ signin.php
@@ -1,31 +1,33 @@
 <!DOCTYPE html>
 <html lang="ru">
-  <head>
-    <meta charset='utf-8'>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Вход</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-  </head>
 
-  <body>
-    <div class="wrapper">
-      <div class="content">
-        <header>
-          <?php include('nav.html') ?>
-        </header>
+<head>
+  <meta charset='utf-8'>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Идентификация</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 
-        <div class="signin">
-          <form class="signin-box" action="signin.php" method="post">
-            <h1>Авторизация</h1>
-            <input type="text" name="" placeholder="Имя пользователя">
-            <input type="password" name="" placeholder="Пароль">
-            <input type="submit" name="" value="Войти">
-          </form>
-        </div>
+<body>
+  <div class="wrapper">
+    <div class="content">
+      <header>
+        <?php include('nav.php') ?>
+      </header>
+
+      <div class="signin">
+        <form class="signin-content" method="post">
+          <h1>Идентификация</h1>
+          <input type="text" name="userName" placeholder="Ваше имя" required />
+          <input type="text" name="userEmail" placeholder="Ваш e-mail" required />
+          <input type="submit" value="Запомнить меня">
+        </form>
       </div>
-
-      <?php include('footer.html') ?>
     </div>
 
-  </body>
+    <?php include('footer.html') ?>
+  </div>
+
+</body>
+
 </html>
