7c74cc07200c480ea36f1328aecb056a3c793a66
Pavel Miskevich <chiki-takipavel@mail.ru>
2020-04-28 15:48:18 +0300

    Добавлен PHPMailer

---
+++ scripts/PHPMailer/Exception.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PHPMailer\PHPMailer;
+
+class Exception extends \Exception
+{
+    public function errorMessage()
+    {
+        return '<strong>' . htmlspecialchars($this->getMessage()) . "</strong><br />\n";
+    }
+}
